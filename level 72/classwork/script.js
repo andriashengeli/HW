77184065a38bab8bf408a3d056c9d50b185f50e6
andriashengeli <andriashengelia5@gmail.com>
2026-01-02 19:29:37 +0400

    commit

---
+++ level 72/classwork/script.js
@@ -0,0 +1,29 @@
+// console log საშუალებით კონსოლში ჩვენ გამოგვაქ რაღაც ტიპის ინფორმაცია იგივეა რაც print ოღონდ js - ში
+
+// 1. number
+
+let age = 14
+let float = 14.5
+let negative = -30
+
+console.log(age)
+console.log(float)
+console.log(negative)
+
+
+// string 
+
+let name = "andria"
+console.log(name)
+
+
+// boolean 
+
+let isactive = true
+let isonlline = false
+
+console.log(isactive)
+console.log(isonlline)
+
+
+
