9425612d0d7d08e91c03b658991e5e023f582bbb
andriashengeli <andriashengelia5@gmail.com>
2026-01-02 18:01:07 +0400

    commit

---
+++ level 72/homework/script2.js
@@ -0,0 +1,57 @@
+// 1. number 
+// გამოიყენება რიცხვებისთვის მაგალითად მთელი რიცხი დაიწერება ასე (20) ხოლო ათწილადი (5.10)
+
+let age = 15
+console.log(age)
+
+let float = 8.99
+console.log(float)
+
+// 2. string
+
+let name = "Andria";
+let message = 'Hello World';
+
+
+
+// 3. boolean 
+// არის მხოლოდ true ან false 
+
+let isOnline = true;
+let isAdmin = false;
+
+
+
+// 4. undefined როცა ცვალდს არაფერი არ აქ მინიჭებული
+
+let x 
+console.log(x)
+// ეს გამოიტანს undefind რადგან არაფერი არ აქვს გადაცემული
+
+// 5. ეს ნიშნავს ცარიელს ანუ არაფერს 
+let user = null
+
+
+// 6. object არის ობიექტი 
+
+
+let car = {
+  brand: "Mercedes",
+  model: "CLS 63",
+  year: 2024
+};
+
+
+
+// 7. Array (Object-ის ტიპია)
+// დალაგებული სია ელემენტების
+let colors = ["red", "black", "white"];
+
+
+
+
+// 8. Function (ასევე Object-ის ტიპია)
+// კოდი, რომელიც ასრულებს მოქმედებას
+function sayHello() {
+  console.log("Hello!");
+}
