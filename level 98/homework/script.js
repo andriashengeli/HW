ebdfcc5d16bf32f3378a1a4da29069a9a912fc04
andriashengeli <andriashengelia5@gmail.com>
2026-02-28 18:14:01 +0400

    commit

---
+++ level 98/homework/script.js
@@ -0,0 +1,20 @@
+const telephone1 = {
+    brand: "iphobe",
+    model: "17 pro max",
+    aboutelephone: function(){
+        console.log("this is " + this.brand + " " + this.model);
+    }
+}
+
+telephone1.aboutelephone();
+
+
+const telephone2 = {
+    brand: "samsung",
+    model: "s24 ultra",
+    aboutelephone: function(){
+        console.log("this is " + this.brand + " " + this.model);
+    }
+}
+
+telephone2.aboutelephone();
